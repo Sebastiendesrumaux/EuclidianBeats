f12faae425365f3dfa1f2b42df5912ebf2cbb25f
Sebastiendesrumaux <Sebastien.desrumaux@gmail.com>
2025-11-27 20:52:39 +0100

    Save 2025-11-27 20:52:39

--- save.sh
+++ save.sh
@@ -1,14 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Rendre le repo sûr si nécessaire
 git config --global --add safe.directory /storage/emulated/0/AndroidIDE/AppProjects/Euclide
 
-# Ajouter tous les fichiers
 git add -A
 
-# Commit horodaté
 msg="Save $(date '+%Y-%m-%d %H:%M:%S')"
 git commit -m "$msg"
 
-# Push vers main
 git push origin main
